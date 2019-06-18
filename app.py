419f75e5cac3026b831f4afbdb8f9361f50a4c64
depretiger <depretiger@gmail.com>
2019-06-18 22:45:14 +0900

    終わり手直し

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def handle_message(event):
         return 
 
     if text == "おわり":
-        line_bot_api.reply_message(event.reply_token, TextSendMessage("finish"))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(mode + "off"))
         filerw("nothing")
         return
 
